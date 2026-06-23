7ccc65a4d2529b0c5839ceb9ee97ebc77fb73c49
Anastasia Kuznetsova <akuznetsova201@gmail.com>
2026-06-23 10:04:34 -0700

    Added true false for average embeddings

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -10,6 +10,7 @@ DEVICE = ("cuda" if torch.cuda.is_available()
           else "mps" if torch.backends.mps.is_available()
           else "cpu" 
           )
+AVERAGE = False
 
 print(f"Loading {MODEL_ID} ...")
 
@@ -25,7 +26,8 @@ def extract_embeddings(
     model,
     layer: str = LAYER_NAME,
     batch_size: int = BATCH_SIZE,
-    pad_id: int = PAD_ID
+    pad_id: int = PAD_ID,
+    average: bool = AVERAGE
 ) -> np.ndarray:
 
 
@@ -36,6 +38,7 @@ def extract_embeddings(
         sequences:  list of DNA strings (A/T/C/G, upper-case recommended)
         layer:      hidden layer nsmr to pool
         batch_size: sequences per forward pass
+        average: if average embeddings across sequence length
     """
       all_embeddings = []
       for start in range(0, len(sequences), batch_size):
@@ -52,23 +55,27 @@ def extract_embeddings(
                                             layer_names = [layer])
             
             hidden = embeddings[layer] # B, L, D
-            mask = torch.zeros((hidden.shape[0], hidden.shape[1]), dtype = torch.bool, device = DEVICE)
-            
-            for i,l in enumerate(lengths):
-                mask[i,:l] = True
-            
-            # Average across sequence length - will remove it later!
-            mask_expanded = mask.unsqueeze(-1).float() # B, L, 1
-            pooled = (hidden * mask_expanded).sum(1)
-            pooled = pooled/mask_expanded.sum()
+
+            if average:
+                    mask = torch.zeros((hidden.shape[0], hidden.shape[1]), dtype = torch.bool, device = DEVICE)
+                    
+                    for i,l in enumerate(lengths):
+                        mask[i,:l] = True
+                    
+                    # Average across sequence length - will remove it later!
+                    mask_expanded = mask.unsqueeze(-1).float() # B, L, 1
+                    pooled = (hidden * mask_expanded).sum(1)
+                    pooled = pooled/mask_expanded.sum()
+            else:
+                pooled = hidden
             all_embeddings.append(pooled.cpu().numpy())
             print(f"Processed {min(start + batch_size, len(sequences))}/{len(sequences)} sequences")
 
       return np.concatenate(all_embeddings, axis = 0)
 
 if __name__ == "__main__":
-    seqs = np.load('output/NPC1_mut_seq.npy')
-    embeddings = extract_embeddings(seqs, model, LAYER_NAME, BATCH_SIZE)
+    seqs = np.load(f"output/NPC1_dna_seq_avg_{AVERAGE}.npy")
+    embeddings = extract_embeddings(seqs, model, LAYER_NAME, BATCH_SIZE, AVERAGE)
     np.save("embeddings.npy", embeddings)
 
                 
