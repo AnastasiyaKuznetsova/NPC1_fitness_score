89eb90965978bff4f9e74b5eea45dab6c66852be
Anastasia Kuznetsova <akuznetsova201@gmail.com>
2026-08-26 15:18:36 -0700

    Added zero shot log likelihood

---
+++ Evo2_zero_shot.py
@@ -0,0 +1,19 @@
+from evo2.models import Evo2
+import numpy as np
+import pandas as pd
+from scipy.stats import spearmanr
+
+model = Evo2("evo2_7b")
+# Let's do for forward
+var_seqs = np.load('output/mut_seq_DNA_forward_20260825.npy')
+ref_seqs = np.load('ref_seq_DNA_forward_20260825.npy')
+df = pd.read_csv('output/df_preprocessed.csv')
+
+print(f'Scoring likelihoods of {len(ref_seqs)} reference sequences with Evo 2...')
+ref_scores = model.score_sequences(ref_seqs)
+
+print(f'Scoring likelihoods of {len(var_seqs)} variant sequences with Evo 2...')
+var_scores = model.score_sequences(var_seqs)
+
+delta_scores = np.array(var_scores) - np.array(ref_scores)
+print(spearmanr(df['Function score'], delta_scores))
