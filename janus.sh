7ff78a2fe60faf5ce77ef064c713bed1cd0bbbf6
Anastasia Kuznetsova <akuznetsova201@gmail.com>
2026-09-09 11:50:33 -0700

    Added juans.sh to run on the server

---
+++ janus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+tmux new-session -s -d janus "srun --reservation=balch -p gpu --pty bash \
+apptainer exec --nv sif/janusdna.sif python extract_embeddings_JanusDNA.py --janusdna-repo JanusDNA \
+--checkpoint Janus_ckpt_json/32_without_midattn.ckpt \
+--ref-file output/20260903_120806/ref_seq_DNA_forward_1024bp.npy \
+--mut-file output/20260903_120806/mut_seq_DNA_forward_1024bp.npy; exec bash"
