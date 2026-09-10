8c5ecb7ca33bb7ed6550d1adeb200508cd6f3418
Anastasia Kuznetsova <akuznetsova201@gmail.com>
2026-09-10 12:32:34 -0700

    Fixed -d -s janus instead of -s -d janus in janus.sh

--- janus.sh
+++ janus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-tmux new-session -s -d janus "srun --reservation=balch -p gpu --pty bash \
+tmux new-session -d -s janus "srun --reservation=balch -p gpu --pty bash \
 apptainer exec --nv sif/janusdna.sif python extract_embeddings_JanusDNA.py --janusdna-repo JanusDNA \
 --checkpoint Janus_ckpt_json/32_without_midattn.ckpt \
 --ref-file output/20260903_120806/ref_seq_DNA_forward_1024bp.npy \
